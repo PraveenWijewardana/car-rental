686cf977cedb73381bd032204a94cd2d22ab887d
Wasuda Sandaruvan <147605725+WasuVa@users.noreply.github.com>
2026-01-29 11:57:06 +0530

    change

--- src/main/java/com/car/service/BookingExtrasService.java
+++ src/main/java/com/car/service/BookingExtrasService.java
@@ -1,9 +1,15 @@
 package com.car.service;
 
+import com.car.dto.BookingExtrasDTO;
 import com.car.dto.BookingsDTO;
+
+import java.util.List;
 
 public interface BookingExtrasService {
     void addBookingExtras(BookingsDTO bookingDTO);
     void updateBookingExtras(BookingsDTO bookingDTO);
     void deleteBookingExtras(Integer id);
+    BookingExtrasDTO findByBookingId(Integer id);
+    BookingExtrasDTO findByServiceId(Integer id);
+    List<BookingExtrasDTO> getAll();
 }
--- src/main/java/com/car/service/impl/BookingExtrasImpl.java
+++ src/main/java/com/car/service/impl/BookingExtrasImpl.java
@@ -1,6 +1,39 @@
 package com.car.service.impl;
 
+import com.car.dto.BookingExtrasDTO;
+import com.car.dto.BookingsDTO;
 import com.car.service.BookingExtrasService;
 
+import java.util.List;
+
 public class BookingExtrasImpl implements BookingExtrasService {
+    @Override
+    public void addBookingExtras(BookingsDTO bookingDTO) {
+
+    }
+
+    @Override
+    public void updateBookingExtras(BookingsDTO bookingDTO) {
+
+    }
+
+    @Override
+    public void deleteBookingExtras(Integer id) {
+
+    }
+
+    @Override
+    public BookingExtrasDTO findByBookingId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public BookingExtrasDTO findByServiceId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<BookingExtrasDTO> getAll() {
+        return List.of();
+    }
 }
